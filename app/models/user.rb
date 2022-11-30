09bc6b69f0a1520030bf32eaec46d7e471387ac6
kawamoto06 <kawamoto0605@gmail.com>
2022-11-30 19:36:00 +0900

    微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   def user_profile_or_empty
     if self.image.attached? == false
-    return "no_image.png"
+    return "no_image2.png"
     else
     return image
     end

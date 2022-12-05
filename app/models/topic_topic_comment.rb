20f748eee75fa397be2297fb3966ca63ea319f49
kawamoto06 <kawamoto0605@gmail.com>
2022-12-05 17:44:49 +0900

    修正

--- app/models/topic_topic_comment.rb
+++ app/models/topic_topic_comment.rb
@@ -1,1 +1,9 @@
+class TopicTopicComment
+  include ActiveModel::Model
+  attr_accessor :name, :topic_comment, :user_id
 
+  validates :name, presence: true
+  validates :topic_comment, presence: true
+
+
+end

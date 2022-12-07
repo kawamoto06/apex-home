2e1aee5163f5b1032aa61f97f51508f6ce35e792
kawamoto06 <kawamoto0605@gmail.com>
2022-12-07 19:57:56 +0900

    簡易DM機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!, only: [:mypage, :edit, :update]
+before_action :authenticate_user!, only: [:index, :edit, :update]
 before_action :set_user, only: [:show, :edit, :update]
 
-  def mypage
+  def index
     redirect_to user_path(current_user)
   end
 
   def show
     @diaries = @user.diaries.order('created_at DESC')
-    @current_entry = Entry.where(user_id: current_user.id)
-    @another_entry = Entry.where(user_id: @user.id)
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
     unless @user.id == current_user.id
-      @current_entry.each do |current|
-        @another_entry.each do |another|
-          if current.room_id == another.room_id
-            @is_room = true
-            @room_id = current.room_id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
           end
         end
       end
-      unless @is_room
+      if @isRoom
+      else
         @room = Room.new
         @entry = Entry.new
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
 
 
   validates :nickname, presence: true
